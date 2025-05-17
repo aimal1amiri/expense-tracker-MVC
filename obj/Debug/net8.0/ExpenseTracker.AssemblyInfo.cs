99374748ea5eb857d3a5b5777b7a5c9041d49716
emir2269 <ali724345@gmail.com>
2025-05-17 20:41:54 +0300

    .

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c271a7982b785170894f4091d4cbd67819413f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb7230e66562368b32a325917cab3bcec4e50fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
