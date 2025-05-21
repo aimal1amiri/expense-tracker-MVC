82a150a110cc694ab5a18f33a0fb0f7b86c519a2
emir2269 <ali724345@gmail.com>
2025-05-21 10:38:42 +0300

    icons

--- obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c133cb0466d3ef36b2a5e55f20ccc8f31e05a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c928b4ca6d6278eafb9f52f1d41f25317776e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
